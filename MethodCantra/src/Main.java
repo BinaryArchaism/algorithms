670dec77635469a4c0c6dc966a439ac19b57ba24
Dmitrii Dmitriev <seriouskeyn@mail.ru>
2018-02-09 22:33:03 +0300

    commit#2

--- MethodCantra/src/Main.java
+++ MethodCantra/src/Main.java
@@ -7,9 +7,43 @@ public class Main {
 
         int n = 6; //размер матрицы
         int matrix[][] = new int[n][n];
+        int h = 1;
 
-        
+        for (int k = 0; k < n; ++k) {
+            if (k%2 != 0) {
+                for (int i = 0; i <= k; ++i) {
+                    matrix[i][k] = h;
+                    ++h;
+                }
+                matrix[k][k] = h;
+                ++h;
+                for (int i = 0; i <= k; ++i) {
+                    matrix[k][i] = h;
+                    ++h;
+                }
+            } else {
+                for (int i = 0; i <= k; ++i) {
+                    matrix[k][i] = h;
+                    ++h;
+                }
+                matrix[k][k] = h;
+                ++h;
+                for (int i = 0; i <= k; ++i) {
+                    matrix[i][k] = h;
+                    ++h;
+                }
+            }
+        }
 
+        print(matrix);
+    }
 
+    public static void print(int matrix[][]) {
+        for (int i = 0; i < matrix.length; ++i) {
+            for (int j = 0; j < matrix.length; ++j) {
+                System.out.print(matrix[i][j] + " | ");
+            }
+            System.out.println();
+        }
     }
 }
