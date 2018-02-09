82a0b595f3469c0bcbc2e41b30f745f6e1a14c19
Dmitrii Dmitriev <seriouskeyn@mail.ru>
2018-02-09 21:04:07 +0300

    commit#1

--- Task_From_06_04_1`8/src/Main.java
+++ Task_From_06_04_1`8/src/Main.java
@@ -21,12 +21,32 @@ public class Main {
         }
 
         //заполнение второй половины без побочной диагонали
-        for (int k = 1; k < n; ++k) {
-            for (int i = n-1; i > n-i+k-1; --i) {
-                matrix[i][i-n+k+1] = h;                 //[проблем нет][ошибка]
+
+        for (int k = n - 1; k > 0; --k) {
+            for (int i = n-k; i > 0; --i) {
+                //меняй местами [][], чтобы изменить направление заполнения
+                //if (k%2!=0)
+                    matrix[i][] = h;
+                //else
+                //    matrix[n-k-i][i] = h;
                 ++h;
             }
         }
+
+//        for (int k = 1; k < n; ++k) {
+//            for (int i = n-1; i > n-i+k-1; --i) {
+//                matrix[i][i-n+k+1] = h;                 //[проблем нет][ошибка]
+//                ++h;
+//            }
+//        }
+
+//        int k = n-1;
+//
+//        for (int j = 1; j < n; ++j) {
+//            int i = n-k;
+//            matrix[k][i] = h;
+//            ++h; --k;
+//        }
 
         print(matrix);
     }
