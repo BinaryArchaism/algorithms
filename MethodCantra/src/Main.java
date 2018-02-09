7a4b8738d894108905b0cb3b92476a2d810cd1b0
Dmitrii Dmitriev <seriouskeyn@mail.ru>
2018-02-09 21:39:50 +0300

    commit#1

---
+++ MethodCantra/src/Main.java
@@ -0,0 +1,15 @@
+/**
+ * @author Dmitrii "BinaryArchaism" Dmitriev
+ */
+
+public class Main {
+    public static void main(String[] args) {
+
+        int n = 6; //размер матрицы
+        int matrix[][] = new int[n][n];
+
+        
+
+
+    }
+}
