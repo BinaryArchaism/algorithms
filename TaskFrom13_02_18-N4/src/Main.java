180a571062c39ac90284cf1632d1c7ebd7796ded
Dmitrii Dmitriev <seriouskeyn@mail.ru>
2018-02-13 19:22:39 +0300

    done

---
+++ TaskFrom13_02_18-N4/src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        int k = 4; // опорный элемент
+        int array[] = {13, 2, k, 6, 3}; // количество = 11
+
+        print(array);
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > k) {
+                int j = array[i];
+                array[i] = array[array.length-1 - i];
+                array[array.length-1 - i] = j;
+            }
+        }
+
+        print(array);
+    }
+
+    public static void print(int matrix[]) {
+            for (int j = 0; j < matrix.length; ++j) {
+                System.out.print(matrix[j] + " | ");
+            }
+        System.out.println();
+    }
+}
