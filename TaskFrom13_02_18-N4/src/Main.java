073fc4be33e121053eced7fe21b38b58155b32ad
Dmitrii Dmitriev <seriouskeyn@mail.ru>
2018-02-26 14:30:03 +0300

    done

--- TaskFrom13_02_18-N4/src/Main.java
+++ TaskFrom13_02_18-N4/src/Main.java
@@ -1,25 +1,42 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        int k = 4; // опорный элемент
-        int array[] = {14,16, k, 1, 3}; // количество = 11
-
+        int n = 15;
+        int array[] = new int[n];
+        fillArray(array);
         print(array);
 
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] < k) {
-                int j = array[i];
-                array[i] = array[array.length-1 - i];
-                array[array.length-1 - i] = j;
+        int k = 4; // опорный элемент
+
+        int i = 0;
+        int j = array.length-1;
+        while (i < j) {
+            if (array[i] <= k && array[j] >= k) {i++; j--;}
+            else if (array[i] >= k && array[j] <= k) {
+                int p = array[i];
+                array[i] = array[j];
+                array[j] = p;
+                i++; j--;
             }
+            else if (array[i] > k) j--;
+            else i++;
         }
 
         print(array);
     }
 
     public static void print(int matrix[]) {
-            for (int j = 0; j < matrix.length; ++j) {
-                System.out.print(matrix[j] + " | ");
-            }
+        for (int j = 0; j < matrix.length; ++j) {
+            System.out.print(matrix[j] + " | ");
+        }
         System.out.println();
     }
+
+    public static void fillArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) (Math.random() * 10);
+        }
+    }
+
 }
